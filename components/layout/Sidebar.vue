10a4d5e4b7186e15a40bcbb7f06f13c905bb30fb
Yahor Yankavets <mr.yankovec@gmail.com>
2024-07-09 23:50:22 +0300

    created nuxt pp

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
+    </NuxtLink>
+  </aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+console.log("layout");
+</script>
+
+<template>
+  <section class="grid">
+    <LayoutSidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
